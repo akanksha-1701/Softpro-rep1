659f17af9561c1622e12d52826051d0174f2f783
Akanksha Singh <as0280134@gmail.com>
2022-07-15 14:40:12 +0530

    First  Commit

---
+++ j.js
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Food</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="./styles.css">
+  </head>
+  <body>
+    
+    <article>
+     
+
+      <div>
+        <img src="istock-819671144.jpg"height="400px" width="1200px"/>
+      </div>
+      <header>
+        <h1>Healthy food lifestyle</h1>
+        
+      </header>
+      <main>
+        <p>
+            A food is something that provides nutrients.Nutrients are substances that provide:energy for activity,growth and all functions of the body such as breathing, digesting food, and keeping warm; materials for the growth and repair of the body, and for keeping the immune system healthy.Eat healthy food and live life to its fullest.
+
+            So our parents and doctors force us to eat healthy food. To achieve good health one should eat healthy. Healthy eating include balanced diet and balanced proportion of carbohydrates, proteins, water and vitamins.
+            
+            Eat healthy and on time so that it can affect you even more. Besides you eat healthy, exercise also plays a very good role in the formation of your health. </p>
+
+        </p>
+            
+            <h2>Benefits of eating healthy food</h2> 
+        <p>
+            
+            Promotes overall health –
+             We get a strong and healthy body with a healthy mind as well by eating healthy food. Healthy food keeps fit and active.
+            Active brain –
+             Healthy food is full of various nutrients. These nutrients provide us energy and alertness. Hence, we stay active.
+            It protects you from various diseases, 
+            including chronic diseases – 
+            We can safeguard us from various dangerous diseases like diabetes, high blood pressure, high cholesterol, etc. by eating healthy food
+            Combat obesity – Healthy food also saves us from obesity as it helps in managing unnecessary weight gain.
+            Strengthens our immune system and digestion – Healthy food strengthens our immune system, and digestion. For example, fiber rich food keeps us digestion smooth and vitamin C improves our immunity.
+            It helps you build an attractive body– Healthy food also gives us a fit and fine body, glowing skin and overall, an attractive body.
+            It lifts your mood and makes you feel good – We never feel lethargic after eating light and healthy food. Rather, we feel active and energetic. Hence, we feel good after eating healthy food.
+            Conclusion – A healthy mind resides in a healthy body. Hence, to attain a healthy mind and a healthy body, we should eat healthy food.
+         
+         
+         
+        </p>
+        <h3>my favourite food</h3>
+        <p>
+            My favourite healthy food is tomatoes. I love tomatoes because they have a high nutrient content like vitamin C and fibre, which makes them one of the best foods to eat if you want to maintain a healthy lifestyle. Tomatoes are also an excellent source of antioxidants that can help protect us from cancer and other diseases.
+        </p>
+        <h2>Good health eating healthy food</h2>
+        <p>
+        <div>fruit</div><br>
+        <div>broccoli</div><br>
+        <div>vegetable</div><br>
+        <div>kale</div>
+        </p>
+      </main>
+      <footer>
+        <div>
+          <h2>Akanksha Singh</h2>
+        <p>
+            this is a healthyfood is frovide the nutrient
+          </p>
+        </div>
+      </footer>
+    </article>
+  </body>
+</html>
